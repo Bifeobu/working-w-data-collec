4385d7f5a8e1c6e088fd1f02778caa5fe79954eb
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-16 18:30:15 -0400

    made second array

--- script.js
+++ script.js
@@ -10,4 +10,12 @@ let csvStrArray = [
 ] 
 
 // Part 2: Expanding Functionality
-const columns = 
+const columns = csvStrArray2[0] 
+
+let csvStrArray2 = [
+    ["ID,Name,Occupation,Age"],
+    ["42,Bruce,Knight,41"],
+    ["57,Bob,Fry Cook,19"],
+    ["63,Blaine,Quiz Master,58"],
+    ["98,Bill,Doctor’s Assistant,26"]
+]
