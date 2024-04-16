b42202b5ee40e492bcce3474b9be38141171c78a
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-16 17:43:28 -0400

    modified

---
+++ script.js
@@ -0,0 +1,13 @@
+// Part 1: Refactoring Old Code
+let csvStrArray = [
+    ["ID,Name,Occupation,Age"],
+    [
+        ["42,Bruce,Knight,41"]
+        ["57,Bob,Fry Cook,19"],
+        ["63,Blaine,Quiz Master,58"],
+        ["98,Bill,Doctor’s Assistant,26"]
+    ]
+] 
+
+// Part 2: Expanding Functionality
+const columns = 
