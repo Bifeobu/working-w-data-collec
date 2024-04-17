0632219d61aef4caf9eec5af6112052d6cbc29df
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-16 20:31:23 -0400

    created objects for part 3

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let csvArray = [
         ["63,Blaine,Quiz Master,58"],
         ["98,Bill,Doctor’s Assistant,26"]
     ]
-] 
+]
 
 // Part 2: Expanding Functionality
-const columns = csvArray2[0] 
+const columns = csvArray2[0]
 
 let csvArray2 = [
     ["ID,Name,Occupation,Age"],
@@ -18,4 +18,33 @@ let csvArray2 = [
     ["57,Bob,Fry Cook,19"],
     ["63,Blaine,Quiz Master,58"],
     ["98,Bill,Doctor’s Assistant,26"]
+]
+
+// Part 3: Transforming Data
+
+const rows = [
+    {
+        id: "42",
+        name: "Bruce",
+        occupation: "Knight",
+        age: "41"
+    },
+    {
+        id: "57",
+        name: "Bob",
+        occupation: "Fry Cook",
+        age: "19"
+    },
+    {
+        id: "63",
+        name: "Blaine",
+        occupation: "Quiz Master",
+        age: "58"  
+    },
+    {
+        id: "98",
+        name: "Bill",
+        occupation: "Doctor's Assistant",
+        age: "26"
+    }
 ]
