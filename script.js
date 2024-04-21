e9dbbf5ba625f67cd68ba17484843fb693f240ca
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-04-21 18:21:47 -0400

    object to array part 5

--- script.js
+++ script.js
@@ -108,3 +108,36 @@ console.log(ageArray)
 //     }
 //     return sum / ageArray.length;
 // }
+
+// Part 5: Ful Circle
+const obj = [
+    {
+        id: "42",
+        name: "Bruce",
+        occupation: "Knight",
+        age: "41"
+    },
+    {
+        id: "57",
+        name: "Bob",
+        occupation: "Fry Cook",
+        age: "19"
+    },
+    {
+        id: "63",
+        name: "Blaine",
+        occupation: "Quiz Master",
+        age: "58"  
+    },
+    {
+        id: "98",
+        name: "Bill",
+        occupation: "Doctor's Assistant",
+        age: "26"
+    }
+]
+const result = Object.keys(obj).map((key) => [key, obj[key]]);
+
+console.log(result);
+
+// const arrayStr = obj
